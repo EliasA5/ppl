0ba0826180fb0a30a5cae36230441595bef9f156
ShadenSimaan99 <simaan@post.bgu.ac.il>
2021-04-08 12:52:17 +0300

    master

--- hw2/src/q4.ts
+++ hw2/src/q4.ts
@@ -1,3 +1,4 @@
+import { map } from 'ramda';
 import { Exp, Program } from '../imp/L3-ast';
 import { Result, makeFailure } from '../shared/result';
 
@@ -7,4 +8,21 @@ Signature: l2ToPython(l2AST)
 Type: [EXP | Program] => Result<string>
 */
 export const l2ToPython = (exp: Exp | Program): Result<string>  => 
-    makeFailure("TODO");
+isBoolExp(exp) ? valueToString(exp.val) :
+isNumExp(exp) ? valueToString(exp.val) :
+isStrExp(exp) ? valueToString(exp.val) :
+isLitExp(exp) ? unparseLitExp(exp) :
+isVarRef(exp) ? exp.var :
+isProcExp(exp) ? unparseProcExp(exp) :
+isIfExp(exp) ? `(if ${unparseL31(exp.test)} ${unparseL31(exp.then)} ${unparseL31(exp.alt)})` :
+isAppExp(exp) ? `(${unparseL31(exp.rator)} ${unparseLExps(exp.rands)})` :
+isPrimOp(exp) ? exp.op :
+isLetExp(exp) ? unparseLetExp(exp) :
+isDefineExp(exp) ? `(define ${exp.var.var} ${unparseL31(exp.val)})` :
+isProgram(exp) ? `(L31 ${unparseLExps(exp.exps)})` :
+exp; 
+
+const unparseProg = (list: Exp[]): Result<string> =>{
+const a = map(l2ToPython, list);
+return a.every(isOk) ? a.map()
+}
