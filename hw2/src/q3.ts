6e4a23849cb5a61e516a57ad0d287883c6eb0950
Elias Assaf <elyas51000@gmail.com>
2021-04-12 04:01:36 +0300

    finished class2proc

--- hw2/src/q3.ts
+++ hw2/src/q3.ts
@@ -1,18 +1,38 @@
-import { /*ClassExp, ProcExp, */ Exp, Program } from "./L31-ast";
-import { Result, makeFailure } from "../shared/result";
+import { ClassExp, Binding, CExp, ProcExp,  Exp, Program, isExp, isProgram, makeProgram, makePrimOp, makeProcExp, makeIfExp, makeVarDecl, makeAppExp, makeVarRef, makeStrExp, makeBoolExp, isClassExp, makeLitExp } from "./L31-ast";
+import { Result, makeFailure, makeOk } from "../shared/result";
+import { map } from 'ramda';
+import { first, rest } from "../shared/list";
 
 /*
 Purpose: Transform ClassExp to ProcExp
 Signature: for2proc(classExp)
 Type: ClassExp => ProcExp
 */
-//export const class2proc = (exp: ClassExp): ProcExp =>
-    //@TODO
+export const class2proc = (exp: ClassExp): ProcExp =>{
+    const vars = exp.fields;
+    const methods = exp.methods;
+    const methods2ifs = (bindings: Binding[]): CExp =>
+        bindings.length !== 0 ? makeIfExp(makeAppExp(makePrimOp('eq?'), [makeVarRef('msg'), makeLitExp(`'${first(bindings).var.var}`)]), makeAppExp(first(bindings).val,[]), methods2ifs(rest(bindings))) :
+                                makeBoolExp(false);
+
+    return makeProcExp(vars, [makeProcExp([makeVarDecl('msg')], [methods2ifs(methods)])]);
+}
+
+
 
 /*
 Purpose: Transform L31 AST to L3 AST
 Signature: l31ToL3(l31AST)
 Type: [Exp | Program] => Result<Exp | Program>
 */
+
 export const L31ToL3 = (exp: Exp | Program): Result<Exp | Program> =>
-    makeFailure("TODO");
+isClassExp(exp) ? makeOk(class2proc(exp)) : makeFailure('@todo');
+/*
+isExp(exp) ? makeOk(rewriteAllClassExp(exp)) :
+isProgram(exp) ? makeOk(makeProgram(map(rewriteAllClassExp, exp.exps))) :
+exp;
+
+const rewriteAllClassExp = (exp: Exp): Result<Exp> =>
+    makeFailure('@todo')
+    */
