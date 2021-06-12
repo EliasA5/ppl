c47d5576fe0cd57ed46e387eebeeda9bbb4202f4
Elias Assaf <elyas51000@gmail.com>
2021-06-13 00:22:46 +0300

    finished Q3.2

--- hw5/ex5/ex5.pl
+++ hw5/ex5/ex5.pl
@@ -1,7 +1,12 @@
 :- module('ex5',
         [author/2,
          genre/2,
-         book/4
+         book/4,
+         authorOfGenre/2,
+         longestBook/2,
+         isBiggerThan/2,
+         versatileAuthor/1,
+         notEqual/2
         ]).
 
 /*
@@ -16,7 +21,6 @@ maximum_printing_depth(100).
    (select(max_depth(_), A, B), ! ; A = B),
    maximum_printing_depth(MPD),
    set_prolog_flag(toplevel_print_options, [max_depth(MPD)|B]).
-
 
 
 author(1, "Isaac Asimov").
@@ -40,3 +44,37 @@ book("The Lord of the Rings", 4, 4, 1250).
 
 % You can add more facts.
 % Fill in the Purpose, Signature as requested in the instructions here
+
+% Signature: authorOfGenre(GenreName, AuthorName)/2
+% Purpose: True if an author with name AuthorName has written a book
+% with the genre GenreName
+authorOfGenre(GenreName, AuthorName) :-
+    book(_X, AuthorID, GenreID, _Y),
+    genre(GenreID , GenreName),
+    author(AuthorID, AuthorName).
+
+% Signature: longestBook(AuthorId, BookName)/2
+% Purpose: True if the longest book written by the author with AuthorId
+% is named BookName
+longestBook(AuthorId, BookName) :-
+    book(BookName, AuthorId, _, Y),
+    findall(L, (book(_,AuthorId,_,L), isBiggerThan(L,Y)), List),
+    isEmptyList(List).
+
+isEmptyList([]).
+
+isBiggerThan(Y, X) :- (Y > X).
+
+% Signature: versatileAuthor(AuthorName)/1
+% Purpose: True if the author with AuthorName has written book in
+% atleast three different genres.
+versatileAuthor(AuthorName) :-
+   author(AuthorId, AuthorName),
+   findall(GenreId, book(_, AuthorId, GenreId, _), List),
+   member(X, List), member(Y, List), member(Z, List),
+   notEqual(X, Y), notEqual(X, Z), notEqual(Y, Z).
+
+notEqual(X, Y) :- not(X = Y).
+
+
+
